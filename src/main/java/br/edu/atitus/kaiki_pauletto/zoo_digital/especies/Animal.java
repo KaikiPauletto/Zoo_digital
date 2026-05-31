948f9ad9f26413b24916ff09e129a0ad8d993ec7
Kaiki Pauletto <kaikipauletto@hotmail.com>
2026-05-31 17:06:00 -0300

    Estuturação da classe Animal.java

--- src/main/java/br/edu/atitus/kaiki_pauletto/zoo_digital/especies/Animal.java
+++ src/main/java/br/edu/atitus/kaiki_pauletto/zoo_digital/especies/Animal.java
@@ -1,18 +1,67 @@
 package br.edu.atitus.kaiki_pauletto.zoo_digital.especies;
 
+
 public abstract class Animal {
 
     private static int contador = 0;
 
-    private String nome;
-    private Integer idade;
-    private String especie;
-
-    public Animal(String nome, Integer idade, String especie) {
-        this.nome = nome;
-        this.idade = idade;
-        this.especie = especie;
-        contador++;
+    public static int getContador() {
+        return contador;
     }
+
+    public final String VERSAO = "2.5";
+
+    // Atributos
+    private String nome;
+    private String especie;
+    private Integer idade;
+
+    // Método Construtor
+    public Animal(String nome, String especie, Integer idade){
+        this.nome = nome; // Inicializa o atributo "nome" com o valor do parâmetro "nome"
+        this.especie = especie;
+        this.idade = idade;
+        Animal.contador++;
+    }
+
+    // Métodos Gets e Sets
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public void comer() {
+        this.comer("alguma coisa");
+    }
+    // Sobrecarga de método - Polimorfismo estático
+    // Dois ou mais métodos com o mesmo identificador, mas assinatura diferentes
+    public final void comer(String alimento) {
+        IO.println(this.getNome() + " está comendo " + alimento);
+    }
+
+    // Sobrescrevendo o método herdado da classe Object
+    // Polimorfismo Dinâmico
+    @Override
+    public String toString() {
+        return "Nome: " + this.getNome() + "; Idade: " + this.getIdade() + " anos.";
+    }
+
+    public abstract void emitirSom();
+}
+
 
 
